8db4533152c23ee1018de3184437d6ea20a353b1
Arison99 <harrisondaviinci@gmail.com>
2025-06-12 01:03:30 +0300

    Create start.py

---
+++ start.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+import os
+import sys
+from app import app
+
+if __name__ == "__main__":
+    # Configure environment for audio
+    os.environ['AUDIODEV'] = 'null'
+    os.environ['ALSA_CONFIG_PATH'] = '/usr/share/alsa/alsa.conf'
+    
+    # Start the Flask app
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
